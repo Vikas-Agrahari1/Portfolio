d7fc89c2f27d45f0f9a487f9e6f07da3b2c88962
Vikas-Agrahari1 <143523575+Vikas-Agrahari1@users.noreply.github.com>
2025-07-27 09:22:01 +0530

    update project link

--- src/data/user.js
+++ src/data/user.js
@@ -40,7 +40,7 @@ const INFO = {
 				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://vikas-agrahari1.github.io/Web_front/",
 		},
 
 		{
@@ -49,7 +49,7 @@ const INFO = {
 				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://swigi-clone-mu.vercel.app/",
 		},
 
 		{
@@ -58,7 +58,7 @@ const INFO = {
 				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://vikas-agrahari1.github.io/Quiz_App/",
 		},
 	],
 };
